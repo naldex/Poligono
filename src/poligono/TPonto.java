753e59aa0170ea3816b509d9a42792e3ed8b7abd
aluno <aluno@192.168.244.155>
2018-02-09 20:11:52 -0200

    Inserção de nomenclatura

--- src/poligono/TPonto.java
+++ src/poligono/TPonto.java
@@ -13,10 +13,13 @@ public class TPonto {
 
     public static void main(String args[]) {
         Ponto p1 = new Ponto();
-        System.out.println (p1.getlocalizacao());
+        System.out.println ("Localização de P1: " + p1.getlocalizacao());
         Ponto p2 = new Ponto (0.0, 8.3);
-        System.out.println(p2.getlocalizacao());
-        System.out.println(p1.getDistancia(p2));
+        System.out.println("Localização de P2: " + p2.getlocalizacao());
+        System.out.println("Distância de P1 para P2: " + p1.getDistancia(p2));
+        Reta r = new Reta (p1,p2);
+        
+        System.out.println("O tamanho da reta r é: " + r.getTamanho());
     }
 
 }
